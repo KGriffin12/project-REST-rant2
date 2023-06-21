348393b12ea207fafc168c173b4c70f100e062f6
Karlie Griffin <113546703+KGriffin12@users.noreply.github.com>
2023-06-20 20:41:58 -0500

    Adding Bootstrap grid to index page

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,13 @@ function error404 () {
           <main>
               <h1>404: PAGE NOT FOUND</h1>
               <p>Oops, sorry, we can't find this page!</p>
+              <div>
+                <img src="/images/cat.jpg" alt="error" />
+              </div>
+                <div>
+                  Photo by <a href="https://unsplash.com/photos/-Y-3eWVK2iY?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink">Tai Bui</a> on 
+                  <a href="https://unsplash.com/photos/-Y-3eWVK2iY?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink">Uplash</a>
+                </div>    
           </main>
       </Def>
     )
--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,13 @@ function home () {
       <Def>
           <main>
               <h1>HOME</h1>
+              <div>
+                <img src="/images/mels.jpg" alt="front of a diner" />
+              </div>
+                <div>
+                  Photo by <a href="https://unsplash.com/photos/k5sV7-drOr8?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink">Kevin Sarduy</a> on 
+                  <a href="https://unsplash.com/photos/k5sV7-drOr8?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink">Uplash</a>
+                </div>
               <a href="/places">
                 <button className="btn-primary">Places Page</button>
               </a>
